a217230249fc27c2b5cbaa63a4676ce5bf18130d
jousley <jousley@jousley-tp.lenexa.ibm.com>
2013-01-19 18:22:40 -0600

    initial commit.

---
+++ src/edu/wpi/first/wpilibj/templates/RobotMap.java
@@ -0,0 +1,29 @@
+package edu.wpi.first.wpilibj.templates;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+    
+    //Joystick Constants
+    public static final int JOYSTICK_PORT = 1;
+    public static final int A_BUTTON = 1;
+    public static final int B_BUTTON = 2;
+    public static final int X_BUTTON = 3;
+    public static final int Y_BUTTON = 4;
+    public static final int LEFT_BUMPER = 5;
+    public static final int RIGHT_BUMPER = 6;
+    public static final int JOYSTICK_BUTTON_1 = 1;
+    public static final int JOYSTICK_BUTTON_2 = 2;
+    public static final int JOYSTICK_BUTTON_3 = 3;
+    public static final int JOYSTICK_BUTTON_4 = 4;
+    public static final int JOYSTICK_BUTTON_5 = 5;
+    public static final int JOYSTICK_BUTTON_6 = 6;
+    //End Joystick Constants
+    
+    public static final int ShooterChannel = 7;
+    
+}
---
+++ src/edu/wpi/first/wpilibj/templates/subsystems/Shooter.java
@@ -0,0 +1,88 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates.subsystems;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStationLCD;
+import edu.wpi.first.wpilibj.DriverStationLCD.Line;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.templates.RobotMap;
+
+/**
+ *
+ * @author jousley
+ */
+public class Shooter extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    private Talon talon;
+    private DriverStationLCD lcd;
+    private double power = 0;
+
+    public Shooter() {
+        super();
+        talon = new Talon(RobotMap.ShooterChannel);
+        lcd = DriverStationLCD.getInstance();
+        
+        printLCD("STOPPED");
+
+
+    }
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+
+    public void increasePower() {
+        if (power < 1.0) {
+            power += .1;
+            power = roundstrip(power);
+        }
+
+        System.out.println("Power set to " + power);
+        printLCD("" + power);
+        talon.set(power);
+    }
+
+    public void decreasePower() {
+        if (power > -1.0) {
+            power -= .1;
+            power = roundstrip(power);
+        }
+
+        System.out.println("Power set to " + power);
+        printLCD("" + power);
+        talon.set(power);
+    }
+
+    public void stop() {
+        power = 0;
+        System.out.println("Power set to " + power);
+        printLCD("STOPPED");
+        talon.set(power);
+    }
+    
+    private void printLCD(String s) {
+        clearLCD();
+        lcd.println(Line.kUser1, 1, s);
+        lcd.updateLCD();
+    }
+    
+    private void clearLCD() {
+        lcd.println(Line.kUser1, 1, "                              ");
+        lcd.updateLCD();
+    }
+    
+    private double roundstrip(double myNum) {
+        
+        int precision = 10; //keep 1 digit
+        myNum = Math.floor(myNum * precision + .5) / precision;
+        return myNum;        
+    }
+    
+}
